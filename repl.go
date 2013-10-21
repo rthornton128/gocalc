23482f307420b8369a3e0277b3d46da0f3205dd8
Rob Thornton <rthornton128@gmail.com>
2013-10-21 08:18:46 -0700

    Fix for line endings on all platforms in REPL

--- repl.go
+++ repl.go
@@ -11,6 +11,18 @@ import (
 
 var version = "0.1"
 
+func stripCR(in []byte) []byte {
+	out := make([]byte, len(in))
+	i := 0
+	for _, ch := range in {
+		if ch != '\r'{
+			out[i] = ch
+			i++
+		}
+	}
+	return out[:i]
+}
+
 func main() {
 	flag.Parse()
 	if flag.NArg() >= 1 {
@@ -18,19 +30,20 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
-		eval.EvalExpr(string(data))
+		eval.EvalExpr(string(stripCR(data)))
 	} else {
 		fmt.Println("Welcome to Calc REPL", version)
 
 		for {
 			fmt.Print(">>>")
 			in := bufio.NewReader(os.Stdin)
-			str, _ := in.ReadString('\n')
-			if str[:len(str)-2] == "q" {
+			b, _ := in.ReadBytes('\n')
+			b = stripCR(b)
+			if len(b) <= 2 && b[0] == 'q' {
 				fmt.Println("QUIT!")
 				break
 			}
-			res := eval.EvalExpr(str)
+			res := eval.EvalExpr(string(b))
 			if res != nil {
 				fmt.Println(res)
 			}
