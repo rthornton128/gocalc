749643682cb5cbaf3a9e86ef17fdceb09a36a871
Rob Thornton <rthornton128@gmail.com>
2013-10-23 14:40:46 -0700

    Initial restructuring of parser in prep for parser improvements.

--- parser/parser.go
+++ parser/parser.go
@@ -22,8 +22,9 @@ func ParseFile(f *token.File, str string) *ast.File {
 
 	p := new(Parser)
 	p.Init(f, str)
+	//p.parse() // will become parseFile()
 	root := ast.NewFile(token.Pos(1), token.Pos(len(str)+1))
-	for n, err := p.next(); ; n, err = p.next() {
+	for n, err := p.parse(); ; n, err = p.parse() {
 		if err != nil {
 			p.file.AddError(err.pos, "Parse: ", err.msg)
 		}
@@ -35,6 +36,7 @@ func ParseFile(f *token.File, str string) *ast.File {
 			break
 		}
 		root.Nodes = append(root.Nodes, n)
+		p.next()
 	}
 	//if p.file.NumErrors() > 0 {
 	//	p.file.PrintErrors()
@@ -46,12 +48,16 @@ func ParseFile(f *token.File, str string) *ast.File {
 type Parser struct {
 	file *token.File
 	scan *scanner.Scanner
+	tok  token.Token
+	pos  token.Pos
+	lit  string
 }
 
 func (p *Parser) Init(file *token.File, expr string) {
 	p.file = file
 	p.scan = new(scanner.Scanner)
 	p.scan.Init(file, expr)
+	p.next()
 }
 
 type perror struct {
@@ -63,56 +69,74 @@ var closeError = errors.New("Unexpected ')'")
 var eofError = errors.New("Reached end of file")
 var openError = errors.New("Opening '(' with no closing bracket.")
 
-func (p *Parser) next() (ast.Node, *perror) {
-	tok, off, lit := p.scan.Scan()
-	pos := p.file.Base() + off
-	//fmt.Println("tok:", tok)
-	//fmt.Println("pos:", pos)
-	//fmt.Println("lit:", lit)
-	open := false
-	switch tok {
-	case token.ADD, token.SUB, token.MUL, token.DIV, token.MOD:
-		return &ast.Operator{pos, lit[0]}, nil
-	case token.IDENT:
-		//fmt.Println("Found identifier", lit, "at pos:", pos)
-		return &ast.Identifier{pos, lit}, nil
+func (p *Parser) next() {
+	p.tok, p.pos, p.lit = p.scan.Scan()
+	p.pos += p.file.Base()
+	//fmt.Println("tok:", p.tok)
+	//fmt.Println("pos:", p.pos)
+	//fmt.Println("lit:", p.lit)
+}
+
+func (p *Parser) parse() (ast.Node, *perror) {
+	var n ast.Node = nil
+	switch p.tok {
+	case token.ADD, token.SUB, token.MUL, token.DIV, token.MOD, token.LT,
+		token.LTE, token.GT, token.GTE, token.EQ, token.NEQ:
+		n = &ast.Operator{p.pos, p.lit}
+	case token.IDENT, token.IF, token.PRINT, token.SET: // temporary
+		n = p.parseIdentifier()
 	case token.NUMBER:
-		i, err := strconv.ParseInt(lit, 0, 64)
-		if err != nil {
-			p.file.AddError(pos, "Parse:", err)
-		}
-		return &ast.Number{pos, lit, int(i)}, nil
+		n = p.parseNumber()
 	case token.LPAREN:
-		open = true
-		e := new(ast.Expression)
-		e.LParen = pos
-		offset := token.Pos(1)
-		for {
-			n, err := p.next()
-			if err != nil {
-				if err.msg == closeError {
-					open = false
-					break
-				}
-			}
-			if n == nil {
+		return p.parseExpression()
+	case token.RPAREN:
+		return nil, &perror{p.pos, closeError}
+	case token.COMMENT:
+		// consume comment and move on
+		p.next()
+		return p.parse()
+	}
+	return n, nil // eofError
+}
+
+func (p *Parser) parseExpression() (*ast.Expression, *perror) {
+	// an lparen was found. scan until an rparen found. determine expression
+	// type: define (arg list, body), if, print and set expressions or
+	// a typical math expression (operator).
+	open := true
+	e := new(ast.Expression)
+	e.LParen = p.pos
+	offset := token.Pos(1)
+	for {
+		p.next()
+		n, err := p.parse()
+		if err != nil {
+			if err.msg == closeError {
+				open = false
 				break
 			}
-			e.Nodes = append(e.Nodes, n)
-			offset = n.End() // - n.Pos()
 		}
-		if open == true {
-			return nil, &perror{pos, openError}
+		if n == nil {
+			break
 		}
-		e.RParen = pos + offset
-		return e, nil
-	case token.RPAREN:
-		if open == false {
-			return nil, &perror{pos, closeError}
-		}
-	case token.COMMENT:
-		//fmt.Println("Found comment:", lit)
-		return p.next()
+		e.Nodes = append(e.Nodes, n)
+		offset = n.End() // - n.Pos()
 	}
-	return nil, nil // eofError
+	if open == true {
+		return nil, &perror{p.pos, openError}
+	}
+	e.RParen = p.pos + offset
+	return e, nil
+}
+
+func (p *Parser) parseIdentifier() *ast.Identifier {
+	return &ast.Identifier{p.pos, p.lit}
+}
+
+func (p *Parser) parseNumber() *ast.Number {
+	i, err := strconv.ParseInt(p.lit, 0, 64)
+	if err != nil {
+		p.file.AddError(p.pos, "Parse:", err)
+	}
+	return &ast.Number{p.pos, p.lit, int(i)}
 }
